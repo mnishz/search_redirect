c29d1446082f069ad295941d726ea0527a2871d5
mnishz <mnishz@users.noreply.github.com>
2020-02-27 19:23:37 +0900

    replace underscore with hyphen

--- jira_redirect.js
+++ jira_redirect.js
@@ -10,7 +10,7 @@ function redirect(requestDetails) {
     if (!pattern.test(requestDetails.url)) return;
     console.log("Redirecting: " + requestDetails.url);
     return {
-        redirectUrl: server_url + requestDetails.url.match(pattern)[0].toUpperCase()
+        redirectUrl: server_url + requestDetails.url.match(pattern)[0].toUpperCase().replace(/_/g, '-')
     };
 }
 
